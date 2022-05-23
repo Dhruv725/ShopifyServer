77e444eafb7a17fb7f8a7f9dca8dc2bbb1411362
Dhruv Soni <98556191+Dhruv725@users.noreply.github.com>
2022-05-23 12:10:50 -0300

    Update index.js
    Adding middlewares

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const cors=require("cors");
 
 //MiddleWare for CORS policy
 app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
+app.use(function(req, res, next) {
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
